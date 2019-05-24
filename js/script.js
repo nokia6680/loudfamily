fdbf71e175ecb7e7a4431d0476bad42a3bb07276
Artur Ballod <artur.ballod@gmail.com>
2019-05-24 23:19:41 +0200

    #320-index-event-full

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var menuShow = document.querySelector(".intro__controls");
-var menuOverlay = document.querySelector(".intro");
 var openButton = document.querySelectorAll(".intro__opener");
+var menuList = document.querySelector(".header");
 var closeButton = document.querySelector(".intro__closer");
 var contentWrap = document.querySelector(".content");
 
@@ -8,32 +8,42 @@ if (menuShow) {
   for (var i = 0; i < openButton.length; i++) openButton[i].addEventListener("click", function(event) {
     event.preventDefault();
     menuShow.classList.add("intro__controls--active");
-    menuOverlay.classList.add("intro--active");
+    menuList.classList.add("header--active");
     closeButton.classList.add("intro__closer--active");
     contentWrap.classList.add("content--active");
   });
 
-  menuOverlay.addEventListener("click", function(event) {
+  menuShow.addEventListener("click", function(event) {
+    event.stopPropagation();
+  });
+
+  menuList.addEventListener("click", function(event) {
     event.stopPropagation();
   });
 
   menuShow.addEventListener("click", function(event) {
     menuShow.classList.remove("intro__controls--active");
+    menuList.classList.remove("header--active");
     contentWrap.classList.remove("content--active");
-    menuOverlay.classList.remove("intro--active");
+  })
+
+  menuList.addEventListener("click", function(event) {
+    menuShow.classList.remove("intro__controls--active");
+    menuList.classList.remove("header--active");
+    contentWrap.classList.remove("content--active");
   })
 
   closeButton.addEventListener("click", function() {
     menuShow.classList.remove("intro__controls--active");
-    menuOverlay.classList.remove("intro--active");
     contentWrap.classList.remove("content--active");
+    menuList.classList.remove("header--active");
   });
 
   window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
       menuShow.classList.remove("intro__controls--active");
-      menuOverlay.classList.remove("intro--active");
       contentWrap.classList.remove("content--active");
+      menuList.classList.remove("header--active");
     }
   });
 }
